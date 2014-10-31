ab0a4631263927f27dd6bcb3b0ed226805e0dbee
slamflipstrom <lindstrom.s@gmail.com>
2014-10-31 16:24:19 -0500

    Javascript WIP

---
+++ themes/my_theme/js/jquery.dropdown.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+  $('#hamburger').click(function(){
+    // $(this).find('.mobile-nav').fadeToggle(400).show();
+    $("movile-nav").slideDown('fast').show();
+  
+  });  
+});
