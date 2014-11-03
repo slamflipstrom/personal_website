92f562fde5b1a147098a22cdcfa9775a19de9fa7
slamflipstrom <lindstrom.s@gmail.com>
2014-11-02 20:38:09 -0600

    Page scroll animation is working

--- themes/my_theme/js/jquery.dropdown.js
+++ themes/my_theme/js/jquery.dropdown.js
@@ -10,5 +10,10 @@ $(document).ready(function(){
     console.log ('hamburger was clicked')
   });
 
+  $(".mobile-nav a").click(function(e){
+    var seg = $(this).attr("href");
+    $("body").animate({scrollTop: $(seg).offset().top}, "slow");
+    e.preventDefault();
+  });
 
 });
