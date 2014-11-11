e076253cf80be5585ee03e4027355b7280a8a6e2
slamflipstrom <lindstrom.s@gmail.com>
2014-11-11 17:27:26 -0600

    Proceed arrow refactoring. Changed image replacement technique

--- themes/my_theme/js/jquery.dropdown.js
+++ themes/my_theme/js/jquery.dropdown.js
@@ -7,22 +7,22 @@ $(document).ready(function(){
     
   $(".drop-icon").click(function(){
     $(".mobile-nav").slideToggle(120);
-    console.log ('hamburger was clicked')
+    console.log ('menu icon was clicked')
   });
 
-    
 
   $(".mobile-nav a").click(function(e){
     var seg = $(this).attr("href");
     $("body").animate({scrollTop: $(seg).offset().top}, "slow");
     e.preventDefault();
-   });
+  });
    
-   $(".proceed-arrow img").click(function(e){
+  $(".proceed-arrow a").click(function(e){
      var seg = $(this).attr("href");
      $("body").animate({scrollTop: $(seg).offset().top}, "slow");
      e.preventDefault();
-    });
+     console.log ('proceed arrow was clicked')
+  });
   
 
 });
