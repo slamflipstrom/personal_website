bf38177559d2ca9bcad47e549d05cf628ec01268
slamflipstrom <lindstrom.s@gmail.com>
2014-11-14 22:06:57 -0600

    inserted HTML into home.php and uploaded to host. WIP

---
+++ themes/my_theme/home.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+Template Name: Home
+*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>SL Portfolio</title>
+  <meta name="description" content="">
+  <!-- <link rel="stylesheet" href="style.css" type="text/css" media="screen" charset="utf-8"> -->
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/sass/style.css" />
+ 
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+  <script type="text/javascript" language="javascript" src="js/jquery.dropdown.js"></script>
+    
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
+</head>
+
+
+<nav>
+  <div class="logo"><a href="#landing-page">Sam Lindstrom!</a></div>
+  <div class="drop-icon"><img src="./images/drop-menu-arrow.png" alt="Drop Menu Icon"/></div>
+  <ul class="mobile-nav">
+    <li><a href="#about">ABOUT</a></li>
+    <li><a href="#work">WORK</a></li>
+    <li><a href="#links">LINKS</a></li>
+    <li><a href="#contact">CONTACT</a></li>
+  </ul>
+</nav>
+
+
+<body>
+  
+  <div id="landing-page">
+    <h1>DEVELOPER, WANNABE DESIGNER</h1>
+    <p>MY NAME'S SAM</p>
+    
+    <div class="proceed-arrow"><a href="#about">Learn more about me!</a></div>
+  </div>
+
+  <div id="about">
+    <div id="about-content">
+     <h2>A LITTLE ABOUT ME</h2>
+     <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+  
+  <div id="skills">
+    <div id="skills-content">
+      <h2>MY SKILLS</h2>
+      <h4 class="sub-head">Backend Development</h4>
+      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h4 class="sub-head">Frontend Development</h4>
+      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h4 class="sub-head">Business</h4>
+      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+  
+  <div id="work">
+    <div id="work-content">
+      <h2>CHECK OUT MY WORK</h2>
+      <div id="project1">
+        <h4 class="sub-head">PROJECT 1</h4>
+        <img src="http://placehold.it/250/71CEE6/000000"></img>
+        <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+      <div id="project2">  
+        <h4 class="sub-head">PROJECT 2</h4>
+        <img src="http://placehold.it/250/71CEE6/000000"></img>
+        <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
+      </div>
+      <div id="project3">
+        <h4 class="sub-head">PROJECT 3</h4>
+        <img src="http://placehold.it/250/71CEE6/000000"></img>
+        <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>  
+    </div>
+  </div>
+  
+  <div id="links">
+    <div id="links-content">
+      <h2>LOOK ME UP</h2>
+      <ul>
+        <li><a href="https://linkedin.com/in/samlindstrom/">LinkedIn</a></li>
+        <li><a href="http://instagram.com/ssssssammmm">Instagram</a></li>
+        <li><a href="https://github.com/slamflipstrom">GitHub</a></li>
+        <li><a href="https://twitter.com/ssssssammmm">Twitter</a></li>
+      </ul>
+      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>    
+    </div>
+  </div>
+  
+  <div id="contact">
+     <h2>DROP ME A LINE</h2>
+    <div id="contact-content">
+      <ul>
+        <li>Phone: (308)888-8888</li>
+        <li>Email: sam@sam.com</li>
+      </ul>
+    </div>
+  </div>
+  <div id="footer">
+    <div class="small-logo"><a href="#landing-page">Sam Lindstrom</a></div>
+  </div>
+</body>
+</html>
+
+
