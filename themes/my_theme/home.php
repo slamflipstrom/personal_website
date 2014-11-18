96cb2b2aa19e27c87ae1a809f4e10c0d45ee525b
slamflipstrom <lindstrom.s@gmail.com>
2014-11-18 15:55:41 -0600

    Replacing filler HTML with actual content

--- themes/my_theme/home.php
+++ themes/my_theme/home.php
@@ -13,7 +13,6 @@ Template Name: Home
   <meta charset="utf-8">
   <title>SL Portfolio</title>
   <meta name="description" content="">
-  <!-- <link rel="stylesheet" href="style.css" type="text/css" media="screen" charset="utf-8"> -->
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
  
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
@@ -23,6 +22,9 @@ Template Name: Home
   <link href='http://fonts.googleapis.com/css?family=Quicksand:300,400' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:300' rel='stylesheet' type='text/css'>
 </head>
+
+
+
 
 
 <nav>
@@ -41,27 +43,27 @@ Template Name: Home
   
   <div id="landing-page">
     <h1>DEVELOPER, WANNABE DESIGNER</h1>
-    <p>MY NAME'S SAM</p>
-    
+    <p>MY NAME'S SAM</p>  
     <div class="proceed-arrow"><a href="#about">Learn more about me!</a></div>
   </div>
 
   <div id="about">
     <div id="about-content">
      <h2>A LITTLE ABOUT ME</h2>
-     <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+     <p class="body-text">A graduate of <a href="www.omahacodeschool.com">Omahe Code School's</a> second ever class, I've been in the web development industry for less than a year. I've found web development to be challenging, intellectually stimulating, and oh so rewarding. I'm passionate about designing intuitive websites, but don't believe form takes precedence over function. I'm thrilled to be a part of the web development industry, and am hungry to broaden my skill set.</p>
     </div>
   </div>
+  
   
   <div id="skills">
     <div id="skills-content">
       <h2>MY SKILLS</h2>
-      <h4 class="sub-head">Backend Development</h4>
-      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <h4 class="sub-head">Frontend Development</h4>
-      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h4 class="sub-head">Back-end Development</h4>
+      <p class="body-text">I have working proficiency in Ruby and Ruby on Rails, as well as some experience with Javascript, Coffeescript, and Sinatra. Additionally, I've worked with PostgreSQL, MySQL, and SQLite. I use Github for all of my personal projects, and am comfortable executing basic tasks in the Command Line.</p>
+      <h4 class="sub-head">Front-end Development</h4>
+      <p class="body-text">I'm skilled with HTML5, CSS3, and object-oriented SASS. I also have familiarity with jQuery. In addition to coding, I am experienced working with Adobe Illustrator, Adobe Photoshop, and Adobe InDesign</p>
       <h4 class="sub-head">Business</h4>
-      <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p class="body-text">I have extensive working experience in business administration and sales. Prior to web development, I graduated with a Bachelor's Degree in Business Administration from the University of Nebraska-Lincoln, and worked in advertising sales for my first post-college job. I invite you to check out my <a href="https://linkedin.com/in/samlindstrom/">LinkedIn</a> page for more details.</p>
     </div>
   </div>
   
@@ -69,17 +71,17 @@ Template Name: Home
     <div id="work-content">
       <h2>CHECK OUT MY WORK</h2>
       <div id="project1">
-        <h4 class="sub-head">PROJECT 1</h4>
+        <h4 class="sub-head">Grupl It</h4>
         <img src="http://placehold.it/250/71CEE6/000000"></img>
-        <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="body-text">An app built to help friends decide upon a night out on the town. My partner handled a bulk of the back-end programming, and I implemented most of the front-end design using SASS primarily. The Javascript and jQuery were collaborative efforts, along with the mobile-responsive user interface. Languages: Ruby on Rails, CSS, SASS, HTML, Javascript, and jQuery.</p>
       </div>
       <div id="project2">  
-        <h4 class="sub-head">PROJECT 2</h4>
+        <h4 class="sub-head">Grant Tracker</h4>
         <img src="http://placehold.it/250/71CEE6/000000"></img>
         <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  
       </div>
       <div id="project3">
-        <h4 class="sub-head">PROJECT 3</h4>
+        <h4 class="sub-head">Task Manager</h4>
         <img src="http://placehold.it/250/71CEE6/000000"></img>
         <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>  
@@ -89,12 +91,14 @@ Template Name: Home
   <div id="links">
     <div id="links-content">
       <h2>LOOK ME UP</h2>
-      <ul>
-        <li><a href="https://linkedin.com/in/samlindstrom/">LinkedIn</a></li>
-        <li><a href="http://instagram.com/ssssssammmm">Instagram</a></li>
-        <li><a href="https://github.com/slamflipstrom">GitHub</a></li>
-        <li><a href="https://twitter.com/ssssssammmm">Twitter</a></li>
-      </ul>
+      <div class="social-media-links">
+        <ul>
+          <li class="linked-in"><a href="https://linkedin.com/in/samlindstrom/">LinkedIn</a></li>
+          <li class="instagram"><a href="http://instagram.com/ssssssammmm">Instagram</a></li>
+          <li class="github"><a href="https://github.com/slamflipstrom">GitHub</a></li>
+          <li class="twitter"><a href="https://twitter.com/ssssssammmm">Twitter</a></li>
+        </ul>
+      </div>
       <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p class="body-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>    
     </div>
@@ -114,5 +118,3 @@ Template Name: Home
   </div>
 </body>
 </html>
-
-
