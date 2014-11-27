37026f202df27662d9e382fe14c58ba8c0939806
slamflipstrom <lindstrom.s@gmail.com>
2014-11-27 12:59:19 -0600

    Nav scrolling with "active" highlights coming along. WIP

--- themes/my_theme/js/jquery.dropdown.js
+++ themes/my_theme/js/jquery.dropdown.js
@@ -21,6 +21,41 @@ jQuery(document).ready(function(){
     jQuery("body").animate({scrollTop: jQuery(seg).offset().top}, "slow");
     e.preventDefault();
   });
+  
+  // Cache selectors
+  var lastId,
+      topMenu = jQuery(".mobile-nav"),
+      topMenuHeight = topMenu.outerHeight()+15,
+      // All list items
+      menuItems = topMenu.find("a"),
+      // Anchors corresponding to menu items
+      scrollItems = menuItems.map(function(){
+        var item = jQuery(jQuery(this).attr("href"));
+        if (item.length) { return item; }
+      });
+
+  // Bind to scroll
+  jQuery(window).scroll(function(){
+     // Get container scroll position
+     var fromTop = jQuery(this).scrollTop()+topMenuHeight;
+   
+     // Get id of current scroll item
+     var cur = scrollItems.map(function(){
+       if (jQuery(this).offset().top < fromTop)
+         return this;
+     });
+     // Get the id of the current element
+     cur = cur[cur.length-1];
+     var id = cur && cur.length ? cur[0].id : "";
+   
+     if (lastId !== id) {
+         lastId = id;
+         // Set/remove active class
+         menuItems
+           .parent().removeClass("active")
+           .end().filter("[href=#"+id+"]").parent().addClass("active");
+     }                   
+  });jQuery
    
   jQuery(".proceed-arrow a").click(function(e){
      var seg = jQuery(this).attr("href");
