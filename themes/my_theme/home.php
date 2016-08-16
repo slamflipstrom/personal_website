1e196c4af8e80dc3fbb3524ba5448816674c1743
Sam Lindstrom <lindstrom.s@gmail.com>
2016-08-15 20:57:10 -0600

    Update Twitter url in Links section

--- themes/my_theme/home.php
+++ themes/my_theme/home.php
@@ -249,7 +249,8 @@ Template Name: Home
           <li class="linked-in"><a href="https://linkedin.com/in/samlindstrom/" target="_blank"><span class="fa fa-linkedin-square fa-3x"><span></a></li>
           <li class="instagram"><a href="http://instagram.com/ssssssammmm" target="_blank"><span class="fa fa-instagram fa-3x"><span></a></li>
           <li class="github"><a href="https://github.com/slamflipstrom" target="_blank"><span class="fa fa-github fa-3x"><span></a></li>
-          <li class="twitter"><a href="https://twitter.com/ssssssammmm" target="_blank"><span class="fa fa-twitter fa-3x"><span></a></li>
+          <li class="twitter"><a href="https://twitter.com/slamflipstrom" target="_blank"><span class="fa fa-twitter fa-3x"><span></a></li>
+          <li class="stack-overflow"><a href="http://stackoverflow.com/users/4589006/sam-lindstrom" target="_blank"><span class="fa fa-stack-overflow fa-3x"><span></a></li>
         </ul>
       </div>
       <p> Check out the above links to see more of my work, and what I've been up to.</p>
